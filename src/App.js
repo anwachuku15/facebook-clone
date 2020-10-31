409b58ec08103bb2dc3d741b81624c4a472dace4
anwachuku15 <anwachuku15@gmail.com>
2020-10-31 15:16:56 -0400

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
       <h1>Facebook Clone</h1>
-      {/* Header */}
+      <Header />
       {/* Sidebar */}
       {/* Feed */}
       {/* Widgets */}
