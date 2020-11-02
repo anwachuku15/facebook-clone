63258ad7a84877e1615a4d428254de4eb1c7f1a0
anwachuku15 <anwachuku15@gmail.com>
2020-11-02 16:44:41 -0500

    added Login component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
 import "./App.css";
 
+import { useStateValue } from "./context/StateProvider";
+import Login from "./components/Login";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Widgets from "./components/Widgets";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+      {user ? (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
