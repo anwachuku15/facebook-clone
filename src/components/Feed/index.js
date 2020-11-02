0cd6a72bbc82724a23182142a5830b184a35a42f
anwachuku15 <anwachuku15@gmail.com>
2020-11-02 18:07:53 -0500

    sync feed to mongoDB

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import { useStateValue } from "../../context/StateProvider";
-import axios from "axios";
+import db from "../../firebase";
+import axios from "../../axios";
 import Pusher from "pusher-js";
 import StoryReel from "./StoryReel";
 import CreatePost from "./CreatePost";
@@ -9,29 +10,39 @@ import Post from "./Post";
 
 const Feed = () => {
   const [{ user }, dispatch] = useStateValue();
+  const [postsData, setPostsData] = useState([]);
+
+  const syncFeed = () => {
+    axios.get("/retrieve/posts").then((res) => {
+      console.log(res.data);
+      setPostsData(res.data);
+    });
+  };
+
+  useEffect(() => {
+    syncFeed();
+  }, []);
+
   return (
     <div className="feed">
       <div className="feed__container">
         <StoryReel />
         <CreatePost />
+        {postsData.map((post) => (
+          <Post
+            profilePic={post.avatar}
+            body={post.text}
+            timestamp={post.timestamp}
+            imgName={post.imgName}
+            username={post.user}
+          />
+        ))}
         <Post
           profilePic={user.photoURL}
           body="This is an example of a post"
           timestamp="1604328116365"
-          imgName="imgName"
           username={user.displayName}
         />
-        {/* {
-                postsData.map(post => (
-                    <Post 
-                        profilePic={post.avatar} 
-                        body={post.body}
-                        timestamp={post.timestamp}
-                        imgName={post.imgName}
-                        user={post.user}
-                    />
-                ))
-            } */}
       </div>
     </div>
   );
