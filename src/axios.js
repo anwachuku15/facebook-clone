0ae6ab0a992343e70f4f7300b9e73188b9713307
anwachuku15 <anwachuku15@gmail.com>
2020-11-02 18:08:27 -0500

    changed baseURL to localhost (development)

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://facebookclone-mern.herokuapp.com/",
+  // baseURL: "https://facebookclone-mern.herokuapp.com/",
+  baseURL: "http://localhost:9000",
 });
 
 export default instance;
