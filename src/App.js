afb2e50a662a9c383a6dbb93b10c41ea6fd28579
anwachuku15 <anwachuku15@gmail.com>
2020-11-02 07:33:09 -0500

    removed h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <h1>Facebook Clone</h1>
       <Header />
       {/* Sidebar */}
       {/* Feed */}
