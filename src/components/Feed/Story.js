4119ac16f69750abd9c5770b61e9d8bd91ceb627
anwachuku15 <anwachuku15@gmail.com>
2020-11-03 11:45:20 -0500

    story styling

--- src/components/Feed/Story.js
+++ src/components/Feed/Story.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./Story.css";
 import { useStateValue } from "../../context/StateProvider";
-import avi from "../../assets/andrew.jpg";
-import aok from "../../assets/aok.jpg";
 
 import { Avatar } from "@material-ui/core";
 const Story = ({ image, profileSrc, title }) => {
   const [{ user }, dispatch] = useStateValue();
   return (
     <div className="story">
-      <div className="story__card"></div>
+      <div style={{ backgroundImage: image }} className="story__bgImage" />
       <Avatar src={user.photoURL} className="story__avatar" />
       <h4>{title}</h4>
     </div>
