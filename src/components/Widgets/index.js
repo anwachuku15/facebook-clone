e973e8d041cdf0410d79ae0a12b6540c25ca1588
anwachuku15 <anwachuku15@gmail.com>
2020-11-03 09:55:23 -0500

    widget styling to contain full iframe

--- src/components/Widgets/index.js
+++ src/components/Widgets/index.js
@@ -5,12 +5,13 @@ const Widgets = () => {
   return (
     <div className="widgets">
       <iframe
+        className="responsive_iframe"
+        style={{ border: "none" }}
         title="COVID-19"
         src="https://anwachuku15.github.io/"
         width="340"
         height="1500"
-        style={{ border: "none", overflow: "hidden" }}
-        scrolling="no"
+        scrolling="yes"
         frameborder="0"
         allowTransparency="true"
         allow="encrypted-media"
