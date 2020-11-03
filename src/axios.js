7ed20a62c5ee4d71fb4e2b06ba5d61150a2addc6
anwachuku15 <anwachuku15@gmail.com>
2020-11-03 11:43:14 -0500

    changed baseURL to heroku

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://facebookclone-mern.herokuapp.com/",
-  baseURL: "http://localhost:9000",
+  baseURL: "https://facebookclone-mern.herokuapp.com/",
+  // baseURL: "http://localhost:9000",
 });
 
 export default instance;
